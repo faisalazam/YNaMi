033ed0d0ae40d8d79811150f78b6c159606987c7
crazy <v-muhammadfaisal@nbnco.com.au>
2018-07-28 23:49:11 +0500

    Adding a blank SpringBoot maven project.

---
+++ src/main/java/pk/lucidxpo/ynami/YNaMiApplication.java
@@ -0,0 +1,12 @@
+package pk.lucidxpo.ynami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YNaMiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YNaMiApplication.class, args);
+	}
+}
---
+++ src/test/java/pk/lucidxpo/ynami/YNaMiApplicationTests.java
@@ -0,0 +1,16 @@
+package pk.lucidxpo.ynami;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YNaMiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
