5a411f998dfd131c8425c90d9f124130f9651bcf
crazy <v-muhammadfaisal@nbnco.com.au>
2018-07-29 02:09:20 +0500

    Added testing support. Added both unit as well as integrations tests as examples. Unit test has two flavors, i.e. one with SpringRunner whereas the other one with MockitoJunitRunner. For controller testing, I guess, the one with SpringRunner can be preferred, as it is running faster than the other one. For other unit tests, like service or repository, MockitoJunitRunner might perform better.
    The @SpringBootTest annotation tells Spring Boot to go and look for a main configuration class (one with @SpringBootApplication for instance), and use that to start a Spring application context.

---
+++ src/test/java/pk/lucidxpo/ynami/controller/WelcomeControllerMockitoTest.java
@@ -0,0 +1,36 @@
+package pk.lucidxpo.ynami.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WelcomeControllerMockitoTest {
+    @InjectMocks
+    private WelcomeController welcomeController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = standaloneSetup(welcomeController).build();
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("welcome"))
+                .andReturn().getResponse();
+    }
+}
