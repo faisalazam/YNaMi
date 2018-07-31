17e3fc036917fce9ddf7fcd5bf2934c89db0c2f2
crazy <v-muhammadfaisal@nbnco.com.au>
2018-08-01 02:55:40 +0500

    Changing the strategy for integration testing, as choosing to use 'MockMvc' instead of 'TestRestTemplate'; reason being rest template doesn't give much option to verify what's being returned in the model (that's something we want to assert while writing tests for 'Controllers'). Now, there'll be no actual server spinning up while running integration tests, as it the http request is mocked and the test will be running in mock server with all the application context loaded.

--- src/test/java/pk/lucidxpo/ynami/AbstractIntegrationTest.java
+++ src/test/java/pk/lucidxpo/ynami/AbstractIntegrationTest.java
@@ -2,19 +2,15 @@ package pk.lucidxpo.ynami;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class AbstractIntegrationTest {
-    @LocalServerPort
-    protected int port;
-
     @Autowired
-    protected TestRestTemplate restTemplate;
+    protected MockMvc mockMvc;
 }
