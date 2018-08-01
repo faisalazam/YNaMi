d77d522b2b2437d49537bbe47f3b56069f8b8cef
crazy <v-muhammadfaisal@nbnco.com.au>
2018-08-02 03:02:45 +0500

    Configured 'StateRepository' bean in TogglzConfiguration class. This is required for persisting the state of the toggles (in db), otherwise changes to the toggles' state will be lost as soon as the server is restarted.
    Note the use of CachingStateRepository, which will act as a cache for persistentStateRepository. It will cache the results for 'cachingStateRepositoryTtl' milliseconds. If you omit the timeout, lookups will be cached until setFeatureState() is called for the specific feature. Using the cache without a timeout only makes sense if the repository state is never modified directly (by modifying the database table for example).

--- src/main/java/pk/lucidxpo/ynami/spring/features/TogglzConfiguration.java
+++ src/main/java/pk/lucidxpo/ynami/spring/features/TogglzConfiguration.java
@@ -1,14 +1,42 @@
 package pk.lucidxpo.ynami.spring.features;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.togglz.core.manager.EnumBasedFeatureProvider;
+import org.togglz.core.repository.StateRepository;
+import org.togglz.core.repository.cache.CachingStateRepository;
+import org.togglz.core.repository.jdbc.JDBCStateRepository;
 import org.togglz.core.spi.FeatureProvider;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class TogglzConfiguration {
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${togglz.table.name}")
+    private String tableName;
+
+    @Value("${togglz.caching.state.repository.ttl}")
+    private Long cachingStateRepositoryTtl;
+
     @Bean
     public FeatureProvider featureProvider() {
         return new EnumBasedFeatureProvider(AvailableFeatures.class);
+    }
+
+    /*
+     * The CachingStateRepository will act as a cache for persistentStateRepository.
+     * It will cache the results for 'cachingStateRepositoryTtl' milliseconds. If you omit the timeout, lookups will be cached
+     * until setFeatureState() is called for the specific feature. Using the cache without a timeout only makes sense
+     * if the repository state is never modified directly (by modifying the database table for example).
+     */
+    @Bean
+    public StateRepository getStateRepository() {
+        final StateRepository persistentStateRepository = new JDBCStateRepository(dataSource, tableName);
+        return new CachingStateRepository(persistentStateRepository, cachingStateRepositoryTtl);
     }
 }
--- src/test/java/pk/lucidxpo/ynami/spring/features/TogglzConfigurationIntegrationTest.java
+++ src/test/java/pk/lucidxpo/ynami/spring/features/TogglzConfigurationIntegrationTest.java
@@ -3,17 +3,40 @@ package pk.lucidxpo.ynami.spring.features;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.togglz.core.manager.EnumBasedFeatureProvider;
+import org.togglz.core.repository.StateRepository;
+import org.togglz.core.repository.jdbc.JDBCStateRepository;
+import org.togglz.core.spi.FeatureProvider;
 import pk.lucidxpo.ynami.AbstractIntegrationTest;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static pk.lucidxpo.ynami.testutils.ReflectionHelper.getField;
 
+@TestPropertySource(properties = {
+        "togglz.table.name=FeatureToggles",
+        "togglz.caching.state.repository.ttl=5000"
+})
 public class TogglzConfigurationIntegrationTest extends AbstractIntegrationTest {
     @Autowired
     private ApplicationContext applicationContext;
 
     @Test
     public void shouldVerifyThatFeatureProviderBeanExists() {
-        assertThat(applicationContext.containsBeanDefinition("featureProvider"), is(true));
+        final FeatureProvider featureProvider = applicationContext.getBean(FeatureProvider.class);
+        assertThat(featureProvider, instanceOf(EnumBasedFeatureProvider.class));
+    }
+
+    @Test
+    public void shouldVerifyThatStateRepositoryBeanExists() throws NoSuchFieldException, IllegalAccessException {
+        final StateRepository stateRepository = applicationContext.getBean(StateRepository.class);
+        assertThat(getField(stateRepository, "ttl"), is(5000L));
+
+        final JDBCStateRepository jdbcStateRepository = (JDBCStateRepository) getField(stateRepository, "delegate");
+        assertThat(jdbcStateRepository, instanceOf(JDBCStateRepository.class));
+        assertThat(getField(jdbcStateRepository, "tableName"), is("FeatureToggles"));
     }
 }
