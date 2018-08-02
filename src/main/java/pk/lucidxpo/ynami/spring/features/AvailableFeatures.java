015b12ba51deff22f0175770591a51e37314caa1
crazy <v-muhammadfaisal@nbnco.com.au>
2018-08-02 17:49:42 +0500

    Enabled METHOD_EXECUTION by default.

--- src/main/java/pk/lucidxpo/ynami/spring/features/AvailableFeatures.java
+++ src/main/java/pk/lucidxpo/ynami/spring/features/AvailableFeatures.java
@@ -1,12 +1,14 @@
 package pk.lucidxpo.ynami.spring.features;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
 
 public enum AvailableFeatures implements Feature {
     @Label("Controller Execution Feature")
     CONTROLLER_EXECUTION,
 
+    @EnabledByDefault
     @Label("Method Execution Feature")
     METHOD_EXECUTION,
 
