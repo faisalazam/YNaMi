94dfa0a4471fbf5d16026a458e231d372a1d2112
crazy <v-muhammadfaisal@nbnco.com.au>
2018-08-09 03:11:24 +0500

    Replaced '@RunWith(SpringRunner.class)' with 'SpringClassRule' and 'SpringMethodRule'. Why???, because in contrast to the 'SpringRunner', Spring's rule-based JUnit support has the advantage that it is independent of any Runner and can therefore be combined with existing alternative runners like JUnit's Parameterized or third-party runners such as the MockitoJUnitRunner. Making this change as, otherwise JUnit only support one Runner at the time.

--- src/test/java/pk/lucidxpo/ynami/AbstractIntegrationTest.java
+++ src/test/java/pk/lucidxpo/ynami/AbstractIntegrationTest.java
@@ -1,19 +1,27 @@
 package pk.lucidxpo.ynami;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@RunWith(SpringRunner.class)
 public class AbstractIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
+
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
 
     @Test
     public void contextLoads() {
