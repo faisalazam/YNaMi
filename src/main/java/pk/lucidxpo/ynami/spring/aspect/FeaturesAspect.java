975f60257f58f589dd9f40138ee126678e3750ba
crazy <v-muhammadfaisal@nbnco.com.au>
2018-08-21 01:24:49 +0500

    Improved logging in FeatureAspect.

--- src/main/java/pk/lucidxpo/ynami/spring/aspect/FeaturesAspect.java
+++ src/main/java/pk/lucidxpo/ynami/spring/aspect/FeaturesAspect.java
@@ -2,8 +2,10 @@ package pk.lucidxpo.ynami.spring.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pk.lucidxpo.ynami.spring.features.FeatureManagerWrappable;
@@ -27,7 +29,12 @@ public class FeaturesAspect {
         if (featureManager.isActive(featureAssociation.value())) {
             return joinPoint.proceed();
         }
-        log.info("Feature " + featureAssociation.value().name() + " is not enabled!");
+        final Signature signature = ((MethodInvocationProceedingJoinPoint) joinPoint).getSignature();
+        log.info(
+                "Execution of '" + signature.getName() + "' method in '" +
+                        signature.getDeclaringTypeName() + "' class is blocked " +
+                        "as the feature '" + featureAssociation.value() + "' is not enabled!"
+        );
         return null;
     }
 }
