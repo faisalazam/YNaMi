286f2fc643dc7ae158815b51b36749cb4f160b1d
crazy <v-muhammadfaisal@nbnco.com.au>
2018-08-29 21:45:10 +0500

    Initialized roleName in builder to fix some integration tests running with mysql.

--- src/test/java/pk/lucidxpo/ynami/persistence/model/security/RoleBuilder.java
+++ src/test/java/pk/lucidxpo/ynami/persistence/model/security/RoleBuilder.java
@@ -2,8 +2,11 @@ package pk.lucidxpo.ynami.persistence.model.security;
 
 import pk.lucidxpo.ynami.persistence.model.AuditableBuilder;
 
+import static pk.lucidxpo.ynami.persistence.model.security.RoleName.values;
+import static pk.lucidxpo.ynami.utils.Randomly.chooseOneOf;
+
 public final class RoleBuilder extends AuditableBuilder<Role, RoleBuilder> {
-    private RoleName name;
+    private RoleName name = chooseOneOf(values());
 
     private RoleBuilder() {
     }
@@ -12,7 +15,7 @@ public final class RoleBuilder extends AuditableBuilder<Role, RoleBuilder> {
         return new RoleBuilder();
     }
 
-    public RoleBuilder withName(RoleName name) {
+    public RoleBuilder withName(final RoleName name) {
         this.name = name;
         return this;
     }
