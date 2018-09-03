9f1329961fb480c368b61d198954575c0ded7823
crazy <v-muhammadfaisal@nbnco.com.au>
2018-09-04 03:20:35 +0500

    Added couple of sample/random test (which will be deleted once there are more real tests) to see that the testing framework is performing as expected.

---
+++ src/test/acceptance/pk/lucidxpo/ynami/acceptance/selenium/test/Login2SeleniumTest.java
@@ -0,0 +1,41 @@
+package pk.lucidxpo.ynami.acceptance.selenium.test;
+
+import org.fluentlenium.core.annotation.Page;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
+import pk.lucidxpo.ynami.acceptance.config.selenium.AbstractSeleniumTest;
+import pk.lucidxpo.ynami.acceptance.pageobjects.LoginPage;
+import pk.lucidxpo.ynami.utils.executionlisteners.DatabaseExecutionListener;
+
+import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+@EnabledIf(value = "${togglz.features.WEB_SECURITY.enabled:false}", loadContext = true)
+@Sql(executionPhase = BEFORE_TEST_METHOD,
+        scripts = {
+                "classpath:insert-roles.sql",
+                "classpath:insert-users.sql"
+        }
+)
+@TestExecutionListeners(value = DatabaseExecutionListener.class, mergeMode = MERGE_WITH_DEFAULTS)
+class Login2SeleniumTest extends AbstractSeleniumTest {
+    @Page
+    private LoginPage loginPage;
+
+    @Test
+    void shouldVerifySuccessfulLoginAndLogoutWithCorrectCredentialsForAdminUser() {
+        loginPage.openPage(port).username("admin").password("admin").submit().logout();
+    }
+
+    @Test
+    void shouldVerifySuccessfulLoginAndLogoutWithCorrectCredentialsForSupportUser() {
+        loginPage.openPage(port).doLogin("support", "support").logout();
+    }
+
+    @Test
+    void shouldVerifySuccessfulLoginAndLogoutWithCorrectCredentialsForUser() {
+        loginPage.openPage(port).doLogin("user", "user").logout();
+    }
+}
