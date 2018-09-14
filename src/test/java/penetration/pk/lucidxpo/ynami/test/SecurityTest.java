79f008686aaac743514e85551d6b88fe6b9fcf77
crazy <v-muhammadfaisal@nbnco.com.au>
2018-09-14 08:12:40 +0500

    Removed ZapManager class. Used classes from zaputils package to start/stop ZAP.

--- src/test/java/penetration/pk/lucidxpo/ynami/test/SecurityTest.java
+++ src/test/java/penetration/pk/lucidxpo/ynami/test/SecurityTest.java
@@ -5,25 +5,42 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import penetration.pk.lucidxpo.ynami.config.Config;
+import penetration.pk.lucidxpo.ynami.zaputils.ZapInfo;
 
-import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import static penetration.pk.lucidxpo.ynami.scanners.ZapManager.getInstance;
+import static java.lang.Thread.sleep;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static penetration.pk.lucidxpo.ynami.config.Config.getInstance;
+import static penetration.pk.lucidxpo.ynami.zaputils.ZapInfo.builder;
+import static penetration.pk.lucidxpo.ynami.zaputils.boot.Zap.startZap;
+import static penetration.pk.lucidxpo.ynami.zaputils.boot.Zap.stopZap;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SecurityTest {
+    private static int port;
+
     @BeforeClass
     public static void setup() throws Exception {
-        getInstance().startZAP(Config.getInstance().getZapPath());
+        final ZapInfo zapInfo = builder().buildToRunZap(getInstance().getZapPath());
+        startZap(zapInfo);
+        port = zapInfo.getPort();
     }
 
     @AfterClass
-    public static void tearDown() throws IOException {
-        getInstance().stopZap();
+    public static void tearDown() {
+        stopZap();
     }
 
     @Test
-    public void doNothing() {
+    public void shouldVerifyZapIsRunning() throws Exception {
+        final String url = "http://localhost:" + port;
+
+        final HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("HEAD");
+        sleep(100);
+        assertEquals(HTTP_OK, conn.getResponseCode());
     }
 }
