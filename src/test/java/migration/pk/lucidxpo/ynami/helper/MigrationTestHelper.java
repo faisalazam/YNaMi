136d7bc8da80fb3a66dfec8193e714998e6f915f
Crazy <faisalazam_1616@hotmail.com>
2023-08-16 14:39:15 +0500

    Fixed some broken db migration tests.

--- src/test/java/migration/pk/lucidxpo/ynami/helper/MigrationTestHelper.java
+++ src/test/java/migration/pk/lucidxpo/ynami/helper/MigrationTestHelper.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class MigrationTestHelper {
     public static final String SCHEMA_NAME = "Test_Schema";
-    public static final String SCHEMA_NAME_PLACEHOLDER_REGEX = "\\$\\{schema_name\\}";
+    public static final String SCHEMA_NAME_PLACEHOLDER_REGEX = "\\$\\{schema_name}";
     public static final String PATCH_DIRECTORY_PATH = "src/main/resources/db/datapatch";
     public static final String SCRIPT_DIRECTORY_PATH = "src/main/resources/db/migration";
 
@@ -34,18 +34,21 @@ public class MigrationTestHelper {
         executeScripts(template, migrationScripts);
     }
 
-    static void evolveDatabaseToPenultimatePoint(final int scriptNumber, final MigrationScriptFetcher fetcher, final MultiSqlExecutor template) throws Exception {
+    static void evolveDatabaseToPenultimatePoint(final int scriptNumber,
+                                                 final MigrationScriptFetcher fetcher,
+                                                 final MultiSqlExecutor template) throws Exception {
         final List<MigrationScript> migrationScripts = fetcher.allMigrationScriptsBefore(scriptNumber, SCRIPT_NUMBERS_TO_IGNORE);
         executeScripts(template, migrationScripts);
     }
 
     public static boolean tableExists(final String tableName, final MultiSqlExecutor executor) {
-        return executor.getTemplate().queryForObject(
+        final Integer count = executor.getTemplate().queryForObject(
                 "SELECT count(*) FROM INFORMATION_SCHEMA.TABLES "
                         + "WHERE upper(TABLE_SCHEMA) = '" + SCHEMA_NAME.toUpperCase() + "' "
                         + "AND upper(TABLE_NAME) = '" + tableName.toUpperCase() + "' "
                 , Integer.class
-        ) > 0;
+        );
+        return count != null && count > 0;
     }
 
     public static boolean columnExists(final String tableName, final String columnName, final MultiSqlExecutor executor) {
@@ -53,17 +56,22 @@ public class MigrationTestHelper {
     }
 
     public static boolean hasColumnWith(final MultiSqlExecutor executor, final String tableName, final String columnName) {
-        return executor.getTemplate().queryForObject(
+        final Integer count = executor.getTemplate().queryForObject(
                 "SELECT count(*) FROM INFORMATION_SCHEMA.COLUMNS "
                         + "WHERE upper(TABLE_SCHEMA) = '" + SCHEMA_NAME.toUpperCase() + "' "
                         + "AND upper(TABLE_NAME) = '" + tableName.toUpperCase() + "' "
                         + "AND upper(COLUMN_NAME) = '" + columnName.toUpperCase() + "' "
                 , Integer.class
-        ) > 0;
+        );
+        return count != null && count > 0;
     }
 
-    public static boolean hasColumnWith(final MultiSqlExecutor executor, final String tableName, final String columnName, final String dataType, final boolean nullable) {
-        return executor.getTemplate().queryForObject(
+    public static boolean hasColumnWith(final MultiSqlExecutor executor,
+                                        final String tableName,
+                                        final String columnName,
+                                        final String dataType,
+                                        final boolean nullable) {
+        final Integer count = executor.getTemplate().queryForObject(
                 "SELECT count(*) FROM INFORMATION_SCHEMA.COLUMNS "
                         + "WHERE upper(TABLE_SCHEMA) = '" + SCHEMA_NAME.toUpperCase() + "' "
                         + "AND upper(TABLE_NAME) = '" + tableName.toUpperCase() + "' "
@@ -71,12 +79,18 @@ public class MigrationTestHelper {
                         + "AND IS_NULLABLE = '" + (nullable ? "YES" : "NO") + "' "
                         + "AND upper(DATA_TYPE) = '" + dataType.toUpperCase() + "' "
                 , Integer.class
-        ) > 0;
+        );
+        return count != null && count > 0;
     }
 
-    public static boolean hasColumnWith(final MultiSqlExecutor executor, final String tableName, final String columnName, final String dataType, final boolean nullable, final int size) {
+    public static boolean hasColumnWith(final MultiSqlExecutor executor,
+                                        final String tableName,
+                                        final String columnName,
+                                        final String dataType,
+                                        final boolean nullable,
+                                        final int size) {
 
-        return executor.getTemplate().queryForObject(
+        final Integer count = executor.getTemplate().queryForObject(
                 "SELECT count(*) FROM INFORMATION_SCHEMA.COLUMNS "
                         + "WHERE upper(TABLE_SCHEMA) = '" + SCHEMA_NAME.toUpperCase() + "' "
                         + "AND upper(TABLE_NAME) = '" + tableName.toUpperCase() + "' "
@@ -85,7 +99,8 @@ public class MigrationTestHelper {
                         + "AND upper(DATA_TYPE) = '" + dataType.toUpperCase() + "' "
                         + "AND " + getSizeColumnName(dataType) + " = '" + size + "'"
                 , Integer.class
-        ) > 0;
+        );
+        return count != null && count > 0;
     }
 
     public static String getSizeColumnName(final String dataType) {
@@ -102,28 +117,33 @@ public class MigrationTestHelper {
                                                    final String tableName,
                                                    final String constrainType,
                                                    final String constraintName) {
-        return executor.getTemplate().queryForObject(
+        final Integer count = executor.getTemplate().queryForObject(
                 "SELECT count(*) FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS "
                         + "WHERE upper(TABLE_SCHEMA) = '" + SCHEMA_NAME.toUpperCase() + "' "
                         + "AND upper(TABLE_NAME) = '" + tableName.toUpperCase() + "' "
                         + "AND upper(CONSTRAINT_TYPE) = '" + constrainType.toUpperCase() + "' "
                         + "AND upper(CONSTRAINT_NAME) = '" + constraintName.toUpperCase() + "' "
                 , Integer.class
-        ) > 0;
+        );
+        return count != null && count > 0;
     }
 
-    public static boolean constraintExistsFor(final MultiSqlExecutor executor, final String tableName, final String columnName, final String columnKey) {
-        return executor.getTemplate().queryForObject(
+    public static boolean constraintExistsFor(final MultiSqlExecutor executor,
+                                              final String tableName,
+                                              final String columnName,
+                                              final String columnKey) {
+        final Integer count = executor.getTemplate().queryForObject(
                 "SELECT count(*) FROM INFORMATION_SCHEMA.COLUMNS "
                         + "WHERE upper(TABLE_SCHEMA) = '" + SCHEMA_NAME.toUpperCase() + "' "
                         + "AND upper(TABLE_NAME) = '" + tableName.toUpperCase() + "' "
                         + "AND upper(COLUMN_NAME) = '" + columnName.toUpperCase() + "' "
                         + "AND COLUMN_KEY = '" + columnKey.toUpperCase() + "' "
                 , Integer.class
-        ) > 0;
+        );
+        return count != null && count > 0;
     }
 
-    private static void executeScripts(MultiSqlExecutor template, List<MigrationScript> migrationScripts) {
+    private static void executeScripts(final MultiSqlExecutor template, final List<MigrationScript> migrationScripts) {
         for (final MigrationScript migrationScript : migrationScripts) {
             System.out.println("==============  Executing migration script " + migrationScript.getFileName() + " ============");
             final String sqlQuery = migrationScript.getContent().replaceAll(SCHEMA_NAME_PLACEHOLDER_REGEX, SCHEMA_NAME);
