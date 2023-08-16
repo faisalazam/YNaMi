693758afc8a3089f93bbaf21167066e9fa2f8f6b
Crazy <faisalazam_1616@hotmail.com>
2023-08-16 18:33:51 +0500

    Formatted couple of sql scripts. Capitalized table names as it seems case sensitive.

--- src/test/resources/insert-roles.sql
+++ src/test/resources/insert-roles.sql
@@ -1,5 +1,11 @@
-DELETE FROM Roles;
+DELETE
+FROM Roles;
 
-INSERT INTO roles(id, name, createdBy, createdDate, lastModifiedBy, lastModifiedDate) VALUES(1001, 'ROLE_USER', 'Test', now(), 'Test', now());
-INSERT INTO roles(id, name, createdBy, createdDate, lastModifiedBy, lastModifiedDate) VALUES(1002, 'ROLE_ADMIN', 'Test', now(), 'Test', now());
-INSERT INTO roles(id, name, createdBy, createdDate, lastModifiedBy, lastModifiedDate) VALUES(1003, 'ROLE_SUPPORT', 'Test', now(), 'Test', now());
+INSERT INTO Roles(id, name, createdBy, createdDate, lastModifiedBy, lastModifiedDate)
+VALUES ('rol_1001', 'ROLE_USER', 'Test', now(), 'Test', now());
+
+INSERT INTO Roles(id, name, createdBy, createdDate, lastModifiedBy, lastModifiedDate)
+VALUES ('rol_1002', 'ROLE_ADMIN', 'Test', now(), 'Test', now());
+
+INSERT INTO Roles(id, name, createdBy, createdDate, lastModifiedBy, lastModifiedDate)
+VALUES ('rol_1003', 'ROLE_SUPPORT', 'Test', now(), 'Test', now());
--- src/test/resources/insert-users.sql
+++ src/test/resources/insert-users.sql
@@ -1,7 +1,23 @@
-INSERT INTO Users (id, name, username, email, password, createdBy, createdDate, lastModifiedBy, lastModifiedDate) VALUES (10021, 'user', 'user', 'user@user.com', '$2a$10$EaGaBu541ZUNS0DpRgyghOBJZYH08U/fX68J88sXVkA7dXJeZ9Ye2', 'Crazy', now(), 'Crazy', now());
-INSERT INTO Users (id, name, username, email, password, createdBy, createdDate, lastModifiedBy, lastModifiedDate) VALUES (10022, 'admin', 'admin', 'admin@admin.com', '$2a$10$UAL4X2jWOVk5EknlXBm6fO0WHPqfrzHKT4JJ.Y799Q/.4awjbYHsa', 'Crazy', now(), 'Crazy', now());
-INSERT INTO Users (id, name, username, email, password, createdBy, createdDate, lastModifiedBy, lastModifiedDate) VALUES (10023, 'support', 'support', 'support@support.com', '$2a$10$znBrUmfgUnZa5J7Ph1KzxeJqhHyvtDP02Wlxtzwf68khYEV3KaAz6', 'Crazy', now(), 'Crazy', now());
+INSERT INTO Users (id, name, username, email, password, createdBy, createdDate, lastModifiedBy, lastModifiedDate)
+VALUES ('uro_10021', 'user', 'user', 'user@user.com', '$2a$10$EaGaBu541ZUNS0DpRgyghOBJZYH08U/fX68J88sXVkA7dXJeZ9Ye2',
+        'Crazy', now(), 'Crazy', now());
 
-INSERT INTO UserRoles (userId, roleId) VALUES (10021, 1001);
-INSERT INTO UserRoles (userId, roleId) VALUES (10022, 1002);
-INSERT INTO UserRoles (userId, roleId) VALUES (10023, 1003);
+INSERT INTO Users (id, name, username, email, password, createdBy, createdDate, lastModifiedBy, lastModifiedDate)
+VALUES ('uro_10022', 'admin', 'admin', 'admin@admin.com',
+        '$2a$10$UAL4X2jWOVk5EknlXBm6fO0WHPqfrzHKT4JJ.Y799Q/.4awjbYHsa',
+        'Crazy', now(), 'Crazy', now());
+
+INSERT INTO Users (id, name, username, email, password, createdBy, createdDate, lastModifiedBy, lastModifiedDate)
+VALUES ('uro_10023', 'support', 'support', 'support@support.com',
+        '$2a$10$znBrUmfgUnZa5J7Ph1KzxeJqhHyvtDP02Wlxtzwf68khYEV3KaAz6', 'Crazy', now(), 'Crazy', now());
+
+
+
+INSERT INTO UserRoles (userId, roleId)
+VALUES ('uro_10021', 'rol_1001');
+
+INSERT INTO UserRoles (userId, roleId)
+VALUES ('uro_10022', 'rol_1002');
+
+INSERT INTO UserRoles (userId, roleId)
+VALUES ('uro_10023', 'rol_1003');
