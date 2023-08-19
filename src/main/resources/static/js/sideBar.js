af80750d37099b9f62dedcf27c80acb3f47637c8
Crazy <faisalazam_1616@hotmail.com>
2023-08-19 10:37:08 +0500

    Fixed some warnings.

--- src/main/resources/static/js/sideBar.js
+++ src/main/resources/static/js/sideBar.js
@@ -1,23 +1,24 @@
 function SideBar() {
     SideBar.prototype.onDocReady = function () {
-        var that = this;
+        const that = this;
         $(".side-bar-toggle").click(function () {
-            that.toggleSideBar(this, 'holyGrail-nav-container', 'fa-angle-double-left fa-angle-double-right');
+            that.toggleSideBar(this,
+                'holyGrail-nav-container',
+                'fa-angle-double-left fa-angle-double-right');
         });
         this.bindClickToMenuDropDowns();
     };
 
     SideBar.prototype.bindClickToMenuDropDowns = function () {
-        var that = this;
-        var dropdown = document.getElementsByClassName("menu-dropdown");
+        const dropdown = document.getElementsByClassName("menu-dropdown");
 
-        var i;
+        let i;
         for (i = 0; i < dropdown.length; i++) {
-            dropdown[i].addEventListener("click", function() {
-                $( this ).parent().find( 'a.active' ).removeClass( 'active' );
+            dropdown[i].addEventListener("click", function () {
+                $(this).parent().find('a.active').removeClass('active');
                 this.classList.toggle("active");
                 $(this).children(":first").toggleClass("fa-caret-down fa-caret-up");
-                var dropdownContent = this.nextElementSibling;
+                const dropdownContent = this.nextElementSibling;
                 $(dropdownContent).slideToggle(1000);
             });
         }
@@ -26,13 +27,13 @@ function SideBar() {
     SideBar.prototype.toggleSideBar = function (toggleSideBarElement, classOfElementToToggle, classesToToggle) {
         $(toggleSideBarElement).children(":first").toggleClass(classesToToggle);
 
-        var elementToToggle = document.getElementsByClassName(classOfElementToToggle)[0];
+        const elementToToggle = document.getElementsByClassName(classOfElementToToggle)[0];
 
-        $(elementToToggle).fadeToggle( 1000);
+        $(elementToToggle).fadeToggle(1000);
     };
 }
 
 $(document).ready(function () {
-    var sideBar = new SideBar();
+    const sideBar = new SideBar();
     sideBar.onDocReady();
 });
