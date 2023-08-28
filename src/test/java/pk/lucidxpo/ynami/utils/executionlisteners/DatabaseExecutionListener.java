0b4adb9958027fbceb170c153aeb338b17267f39
Crazy <faisalazam_1616@hotmail.com>
2023-08-28 16:15:53 +0500

    Made clean db data logic clearer.

--- src/test/java/pk/lucidxpo/ynami/utils/executionlisteners/DatabaseExecutionListener.java
+++ src/test/java/pk/lucidxpo/ynami/utils/executionlisteners/DatabaseExecutionListener.java
@@ -2,75 +2,59 @@ package pk.lucidxpo.ynami.utils.executionlisteners;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.DatabaseMetaDataCallback;
-import org.springframework.jdbc.support.MetaDataAccessException;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import java.util.Set;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static org.springframework.jdbc.support.JdbcUtils.extractDatabaseMetaData;
+import static java.util.stream.Collectors.toSet;
+import static org.springframework.test.jdbc.JdbcTestUtils.deleteFromTables;
 
 public class DatabaseExecutionListener implements TestExecutionListener {
-    private static final String HIBERNATE_SEQUENCE = "hibernate_sequence";
     private static final String FLYWAY_SCHEMA_HISTORY = "flyway_schema_history";
 
     @Override
-    public void beforeTestClass(@SuppressWarnings("NullableProblems") final TestContext testContext) throws Exception {
+    public void beforeTestClass(@SuppressWarnings("NullableProblems") final TestContext testContext) {
         cleanDBData(testContext);
     }
 
     @Override
-    public void afterTestMethod(@SuppressWarnings("NullableProblems") final TestContext testContext)
-            throws MetaDataAccessException, SQLException {
+    public void afterTestMethod(@SuppressWarnings("NullableProblems") final TestContext testContext) {
         cleanDBData(testContext);
     }
 
-    private void cleanDBData(final TestContext testContext) throws MetaDataAccessException, SQLException {
+    private void cleanDBData(final TestContext testContext) {
         final DataSource dataSource = testContext.getApplicationContext().getBean(DataSource.class);
         final JdbcTemplate jdbcTemplate = testContext.getApplicationContext().getBean(JdbcTemplate.class);
         cleanDBData(dataSource, jdbcTemplate);
     }
 
-    private void cleanDBData(final DataSource dataSource,
-                             final JdbcTemplate jdbcTemplate) throws MetaDataAccessException, SQLException {
+    private void cleanDBData(final DataSource dataSource, final JdbcTemplate jdbcTemplate) {
         // The next few statements will find all the tables in our db schema
         // Without being specific and not specifying catalog/schema names, will sometimes result in
         // issues. For example, when root user is used to connect to db, it might also get other tables like
         // sys_config and later on complain that it can't delete from there...
         // Catalog/Schema names can be hard-coded as well...
-        final String catalogAndSchemaName = dataSource instanceof HikariDataSource
+        final String schemaName = dataSource instanceof HikariDataSource
                 ? ((HikariDataSource) dataSource).getPoolName()
                 : null;
-        final DatabaseMetaDataCallback<ResultSet> action = databaseMetaData -> databaseMetaData.getTables(
-                catalogAndSchemaName,
-                catalogAndSchemaName,
-                null,
-                new String[]{"TABLE"}
-        );
-        final ResultSet resultSet = extractDatabaseMetaData(dataSource, action);
-        jdbcTemplate.batchUpdate(getStrings(resultSet).toArray(new String[]{}));
+
+        // The query ["SHOW TABLES FROM " + schemaName] could have been used too, but it has different structure of the
+        // result set for both the H2 and MySql, which will add unnecessary extra logic to make it work for both.
+        final String query = "SELECT TABLE_NAME\n" +
+                "FROM INFORMATION_SCHEMA.TABLES\n" +
+                "WHERE TABLE_SCHEMA = '" + schemaName + "'";
+        final Set<String> tableNames = jdbcTemplate.queryForList(query, String.class)
+                .stream()
+                .filter(tableName -> !FLYWAY_SCHEMA_HISTORY.equalsIgnoreCase(tableName))
+                .collect(toSet());
+        cleanDBData(jdbcTemplate, tableNames);
     }
 
-    private List<String> getStrings(final ResultSet resultSet) throws SQLException {
-        final List<String> statements = newArrayList("SET FOREIGN_KEY_CHECKS = 0;");
-        while (resultSet.next()) {
-            final String tableName = resultSet.getString("TABLE_NAME");
-            addDeleteTableStatement(statements, tableName);
-        }
-        statements.add("SET FOREIGN_KEY_CHECKS = 1;");
-        return statements;
-    }
-
-    private void addDeleteTableStatement(final List<String> statements, final String tableName) {
-        if (!(FLYWAY_SCHEMA_HISTORY.equalsIgnoreCase(tableName)
-                || HIBERNATE_SEQUENCE.equalsIgnoreCase(tableName))
-        ) {
-            statements.add("DELETE FROM " + tableName);
-        }
+    private void cleanDBData(final JdbcTemplate jdbcTemplate, final Set<String> tableNames) {
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0;"); // Disable foreign key constraint checks
+        deleteFromTables(jdbcTemplate, tableNames.toArray(new String[]{}));
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1;"); // Enable foreign key constraint checks
     }
 }
