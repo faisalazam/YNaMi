d88cc5f59f43a4bd4636d0a548ad419d94a2919d
Crazy <faisalazam_1616@hotmail.com>
2023-08-29 17:15:25 +0500

    Fixing the issues with cucumber and maven, as when it is run through maven, no tests get found. So, as part of the fix, trying to remove it dependency on junit 4.

--- src/test/java/acceptance/pk/lucidxpo/ynami/cucumber/test/CucumberTest.java
+++ src/test/java/acceptance/pk/lucidxpo/ynami/cucumber/test/CucumberTest.java
@@ -1,50 +1,43 @@
 package acceptance.pk.lucidxpo.ynami.cucumber.test;
 
-import acceptance.pk.lucidxpo.ynami.cucumber.config.CucumberContextConfigurationLoader;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.runner.RunWith;
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectDirectories;
+import org.junit.platform.suite.api.Suite;
 
-import java.sql.SQLException;
-
-import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CUCUMBER_CONTEXT_LOADER;
-import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CUCUMBER_HTML_REPORTS_DIR;
-import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CUCUMBER_JSON_REPORT;
-import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CUCUMBER_RERUN_REPORT;
-import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CUCUMBER_STEPS_PACKAGE;
+import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CAMELCASE;
+import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CUCUMBER_GLUE_LOCATION;
+import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.CUCUMBER_REPORTING_PLUGINS;
 import static acceptance.pk.lucidxpo.ynami.cucumber.test.CucumberTest.FEATURE_FILES_LOCATION;
-import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.SNIPPET_TYPE_PROPERTY_NAME;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        snippets = CAMELCASE,
-        features = {
-                FEATURE_FILES_LOCATION
-        },
-        glue = {
-                CUCUMBER_STEPS_PACKAGE,
-                CUCUMBER_CONTEXT_LOADER
-        },
-        plugin = {
-                "pretty",
-                CUCUMBER_JSON_REPORT,
-                CUCUMBER_RERUN_REPORT,
-                CUCUMBER_HTML_REPORTS_DIR
-        }
-)
-public class CucumberTest extends CucumberContextConfigurationLoader {
-    static final String FEATURE_FILES_LOCATION = "classpath:cuke/feature/";
-    static final String CUCUMBER_RERUN_REPORT = "rerun:target/cucumber-api-rerun.txt";
-    static final String CUCUMBER_HTML_REPORTS_DIR = "html:target/cucumber-reports/html";
-    static final String CUCUMBER_JSON_REPORT = "json:target/cucumber-reports/json/Cucumber.json";
-    static final String CUCUMBER_STEPS_PACKAGE = "acceptance.pk.lucidxpo.ynami.cucumber.steps";
-    static final String CUCUMBER_CONTEXT_LOADER = "acceptance.pk.lucidxpo.ynami.cucumber.config";
-
-    @AfterAll
-    public static void tearDown() throws SQLException {
-        connection.close();
-        ((AnnotationConfigServletWebServerApplicationContext) applicationContext).close();
-    }
+@Suite
+@IncludeEngines("cucumber")
+@SelectDirectories(FEATURE_FILES_LOCATION)
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = CAMELCASE)
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = CUCUMBER_GLUE_LOCATION)
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = CUCUMBER_REPORTING_PLUGINS)
+// TODO: well designed Cucumber implementation: https://www.swtestacademy.com/selenium-spring-boot-cucumber-junit5-project/
+public class CucumberTest {
+    static final String CAMELCASE = "camelcase";
+    static final String FEATURE_FILES_LOCATION = "src/test/resources/cuke/feature/";
+    static final String CUCUMBER_GLUE_LOCATION = "acceptance.pk.lucidxpo.ynami.cucumber";
+    private static final String CUCUMBER_REPORTS_PATH = "target/test-results/cucumber-reports/acceptance";
+    private static final String CUCUMBER_JSON_REPORT = "json:" + CUCUMBER_REPORTS_PATH + "/json/cucumber.json";
+    private static final String CUCUMBER_HTML_REPORT = "html:" + CUCUMBER_REPORTS_PATH + "/html/cucumber.html";
+    private static final String CUCUMBER_RERUN_REPORT = "rerun:" + CUCUMBER_REPORTS_PATH + "/cucumber-api-rerun.txt";
+    static final String CUCUMBER_REPORTING_PLUGINS = "pretty"
+            + ", " + CUCUMBER_JSON_REPORT
+            + ", " + CUCUMBER_RERUN_REPORT
+            + ", " + CUCUMBER_HTML_REPORT;
+//
+//    TODO: See how we can enable the following method which got commented after all these upgrades???
+//    connection is coming from CucumberContextConfigurationLoader
+//    @AfterAll
+//    public static void tearDown() throws SQLException {
+//        connection.close();
+//        ((AnnotationConfigServletWebServerApplicationContext) applicationContext).close();
+//    }
 }
--- src/test/java/penetration/pk/lucidxpo/ynami/test/SecurityTest.java
+++ src/test/java/penetration/pk/lucidxpo/ynami/test/SecurityTest.java
@@ -1,67 +1,63 @@
 package penetration.pk.lucidxpo.ynami.test;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.runner.RunWith;
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
-import penetration.pk.lucidxpo.ynami.config.cucumber.CucumberContextConfigurationLoader;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectDirectories;
+import org.junit.platform.suite.api.Suite;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
-import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
-import static penetration.pk.lucidxpo.ynami.test.SecurityTest.CUCUMBER_CONTEXT_LOADER;
-import static penetration.pk.lucidxpo.ynami.test.SecurityTest.CUCUMBER_STEPS_CONFIG_PACKAGE;
-import static penetration.pk.lucidxpo.ynami.test.SecurityTest.CUCUMBER_STEP_DEFS_PACKAGE;
+import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.SNIPPET_TYPE_PROPERTY_NAME;
+import static penetration.pk.lucidxpo.ynami.test.SecurityTest.CAMELCASE;
+import static penetration.pk.lucidxpo.ynami.test.SecurityTest.CUCUMBER_GLUE_LOCATION;
+import static penetration.pk.lucidxpo.ynami.test.SecurityTest.CUCUMBER_REPORTING_PLUGINS;
 import static penetration.pk.lucidxpo.ynami.test.SecurityTest.FEATURE_FILES_LOCATION;
-import static penetration.pk.lucidxpo.ynami.test.SecurityTest.PEN_TESTS_JSON_REPORT;
-import static penetration.pk.lucidxpo.ynami.test.SecurityTest.PEN_TESTS_JUNIT_REPORT;
+import static penetration.pk.lucidxpo.ynami.test.SecurityTest.TAGS;
 import static penetration.pk.lucidxpo.ynami.utils.CucumberReportsGenerator.generateReports;
 import static penetration.pk.lucidxpo.ynami.web.drivers.DriverFactory.quitAll;
 import static penetration.pk.lucidxpo.ynami.zaputils.boot.Zap.stopZap;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        snippets = CAMELCASE,
-//        tags = {
-//                "not @wip",
-//                "not @slow"
-//        },
-        tags = "not @wip and not @slow",
-        features = {
-                FEATURE_FILES_LOCATION
-        },
-        plugin = {
-                "pretty",
-                PEN_TESTS_JSON_REPORT,
-                PEN_TESTS_JUNIT_REPORT
-        },
-        glue = {
-                CUCUMBER_CONTEXT_LOADER,
-                CUCUMBER_STEP_DEFS_PACKAGE,
-                CUCUMBER_STEPS_CONFIG_PACKAGE
-        }
-)
-public class SecurityTest extends CucumberContextConfigurationLoader {
-    private static final String CUCUMBER_REPORTS_PATH = "target/reports/security/cucumber";
+@Suite
+@IncludeEngines("cucumber")
+@SelectDirectories(FEATURE_FILES_LOCATION)
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = TAGS)
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = CAMELCASE)
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = CUCUMBER_GLUE_LOCATION)
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = CUCUMBER_REPORTING_PLUGINS)
+public class SecurityTest {
+    static final String CAMELCASE = "camelcase";
+    static final String TAGS = "not @wip and not @slow";
+    static final String CUCUMBER_GLUE_LOCATION = "penetration.pk.lucidxpo.ynami";
+    static final String FEATURE_FILES_LOCATION = "src/test/resources/cuke/security/features/";
+
+    private static final String CUCUMBER_REPORTS_PATH = "target/test-results/cucumber-reports/security";
     private static final String CUCUMBER_HTML_REPORTS_PATH = CUCUMBER_REPORTS_PATH + "/html";
     private static final String CUCUMBER_JSON_REPORT_PATH = CUCUMBER_REPORTS_PATH + "/json-report.json";
 
-    static final String PEN_TESTS_JSON_REPORT = "json:" + CUCUMBER_JSON_REPORT_PATH;
-    static final String FEATURE_FILES_LOCATION = "classpath:cuke/security/features/";
-    static final String PEN_TESTS_JUNIT_REPORT = "junit:target/reports/security/pen_tests.xml";
-    static final String CUCUMBER_STEP_DEFS_PACKAGE = "penetration.pk.lucidxpo.ynami.steps.defs";
-    static final String CUCUMBER_CONTEXT_LOADER = "penetration.pk.lucidxpo.ynami.config.cucumber";
-    static final String CUCUMBER_STEPS_CONFIG_PACKAGE = "penetration.pk.lucidxpo.ynami.steps.config";
+    private static final String PEN_TESTS_JSON_REPORT = "json:" + CUCUMBER_JSON_REPORT_PATH;
+    private static final String PEN_TESTS_JUNIT_REPORT = "junit:" + CUCUMBER_REPORTS_PATH + "/pen_tests.xml";
+    private static final String CUCUMBER_HTML_REPORT = "html:" + CUCUMBER_HTML_REPORTS_PATH + "/cucumber.html";
+
+    static final String CUCUMBER_REPORTING_PLUGINS = "pretty"
+            + ", " + PEN_TESTS_JSON_REPORT
+            + ", " + PEN_TESTS_JUNIT_REPORT
+            + ", " + CUCUMBER_HTML_REPORT;
 
     @AfterAll
-    public static void tearDown() throws IOException, SQLException {
+    public static void tearDown() throws IOException {
+        // TODO: do I need to generate reports explicitly? I think the ones generated through maven will be good enough.
+        //  Or considerinf combing the reports from cluecumber-report-plugin and maven-cucumber-reporting plugins.
         generateReports(CUCUMBER_HTML_REPORTS_PATH, CUCUMBER_JSON_REPORT_PATH);
         quitAll();
         stopZap();
 
-        connection.close();
-        ((AnnotationConfigServletWebServerApplicationContext) applicationContext).close();
+//    TODO: See how we can enable the following method which got commented after all these upgrades???
+//    connection is coming from CucumberContextConfigurationLoader
+//        connection.close();
+//        ((AnnotationConfigServletWebServerApplicationContext) applicationContext).close();
     }
 }
