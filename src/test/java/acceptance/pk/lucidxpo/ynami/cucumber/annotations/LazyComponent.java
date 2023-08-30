a35e095e1eb7b5c1b89416b3fa1ea437765ce3b2
Crazy <faisalazam_1616@hotmail.com>
2023-08-30 22:16:15 +0500

    Added few annotations which will be used for lazy loading of components/configurations/autowiring.

---
+++ src/test/java/acceptance/pk/lucidxpo/ynami/cucumber/annotations/LazyComponent.java
@@ -0,0 +1,33 @@
+package acceptance.pk.lucidxpo.ynami.cucumber.annotations;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+/**
+ * By default, Spring creates all singleton beans eagerly at the startup/bootstrapping of the application context.
+ * The reason behind this is simple: to avoid and detect all possible errors immediately rather than at runtime.
+ * <p>
+ * However, there are cases when we need to create a bean, not at the application context startup, but when we request it.
+ * <p>
+ * Hence the use of the @Lazy annotation. In order to initialize a lazy bean, we reference it from another one with
+ * both @Lazy and @Autowired annotations and the bean itself is marked with both @Lazy and @Component annotations.
+ * <p>
+ * Here we are creating our own combined @LazyComponent annotation to achieve the same result.
+ */
+@Lazy
+@Component
+@Documented
+@Target({TYPE})
+@Retention(RUNTIME)
+@Scope(SCOPE_PROTOTYPE)
+public @interface LazyComponent {
+}
