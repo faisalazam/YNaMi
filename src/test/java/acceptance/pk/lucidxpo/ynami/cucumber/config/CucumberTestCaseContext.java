7e77f0499397c5a91048d43382b1b074da5107e9
Crazy <faisalazam_1616@hotmail.com>
2023-08-30 22:19:28 +0500

    Added WebDriverHooks to quit the webdriver at the end of the scenario.

--- src/test/java/acceptance/pk/lucidxpo/ynami/cucumber/config/CucumberTestCaseContext.java
+++ src/test/java/acceptance/pk/lucidxpo/ynami/cucumber/config/CucumberTestCaseContext.java
@@ -2,17 +2,21 @@ package acceptance.pk.lucidxpo.ynami.cucumber.config;
 
 import acceptance.pk.lucidxpo.ynami.common.config.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 @TestConfiguration
 public class CucumberTestCaseContext {
-    private static final String CUCUMBER_GLUE_SCOPE = "cucumber-glue";
-
-    @Scope(CUCUMBER_GLUE_SCOPE)
-    @Bean(destroyMethod = "quit")
-    public WebDriver webDriver() {
-        return WebDriverFactory.getDriver();
-    }
+//    @Autowired
+//    protected WebDriver driver;
+//    private static final String CUCUMBER_GLUE_SCOPE = "cucumber-glue";
+//
+//    @Scope(CUCUMBER_GLUE_SCOPE)
+//    @Bean(destroyMethod = "quit")
+//    public WebDriver webDriver() {
+//        return driver;
+//        return WebDriverFactory.getDriver();
+//    }
 }
