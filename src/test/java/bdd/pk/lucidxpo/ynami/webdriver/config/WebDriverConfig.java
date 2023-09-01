fce87c5b175e4e5c7a94fd47a562df9c714a20e1
Crazy <faisalazam_1616@hotmail.com>
2023-09-01 20:13:35 +0500

    Added WebDriverManager dependency.

--- src/test/java/bdd/pk/lucidxpo/ynami/webdriver/config/WebDriverConfig.java
+++ src/test/java/bdd/pk/lucidxpo/ynami/webdriver/config/WebDriverConfig.java
@@ -2,12 +2,11 @@ package bdd.pk.lucidxpo.ynami.webdriver.config;
 
 import bdd.pk.lucidxpo.ynami.annotations.LazyConfiguration;
 import bdd.pk.lucidxpo.ynami.webdriver.annotations.WebdriverBeanScope;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -15,6 +14,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+import static io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver;
 import static java.time.Duration.ofSeconds;
 
 @Profile("!grid")
@@ -24,12 +25,16 @@ public class WebDriverConfig {
     @WebdriverBeanScope
     @ConditionalOnProperty(name = "browser", havingValue = "firefox")
     public WebDriver firefoxDriver() {
-        final FirefoxOptions firefoxOptions = new FirefoxOptions();
+        final WebDriverManager webDriverManager = firefoxdriver();
+        final FirefoxOptions options = new FirefoxOptions();
         final Proxy proxy = new Proxy();
         proxy.setAutodetect(false);
         proxy.setNoProxy("no_proxy-var");
-        firefoxOptions.setCapability("proxy", proxy);
-        return new FirefoxDriver(firefoxOptions);
+        options.setCapability("proxy", proxy);
+        webDriverManager
+                .capabilities(options)
+                .setup();
+        return webDriverManager.create();
     }
 
     @Primary
@@ -43,17 +48,20 @@ public class WebDriverConfig {
     @WebdriverBeanScope
     @ConditionalOnProperty(name = "browser", havingValue = "chrome")
     public WebDriver chromeDriver() {
-//        WebDriverManager.chromedriver().setup();
+        final WebDriverManager webDriverManager = chromedriver();
         final ChromeOptions options = new ChromeOptions();
         options.addArguments("--test-type");
         options.setAcceptInsecureCerts(true);
-        return new ChromeDriver(options);
+        webDriverManager
+                .capabilities(options)
+                .setup();
+        return webDriverManager.create();
     }
 
     @Primary
     @WebdriverBeanScope
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(name = "browser", havingValue = "htmlUnit")
+    @ConditionalOnProperty(name = "browser", havingValue = "htmlunit")
     public WebDriver htmlUnitDriver() {
         final WebDriver driver = new HtmlUnitDriver(true);
         driver.manage().timeouts().implicitlyWait(ofSeconds(2));
