ff5a477603281eff7ed6ea425a8eb250b2e2796e
Crazy <faisalazam_1616@hotmail.com>
2023-09-05 17:45:29 +0500

    Upgrading ZAP from 2.6.0 to 2.13.0.

---
+++ readme/ZAP.md
@@ -0,0 +1,27 @@
+
+
+https://www.zaproxy.org/faq/how-can-you-start-zap/
+
+Generally, most user’s tend to use the Mac OS build, which is a ordinary Mac OS app that can be started as any
+other app: Double-Click on the app to start it.
+
+If you have installed ZAP in ‘/Applications’ then you can run it from the command line using
+‘/Applications/OWASP\ ZAP.app/Contents/Java/zap.sh’
+
+If you have installed ZAP in another directory then change the initial ‘/Applications’ part accordingly.
+
+If you have downloaded the Linux package, which can also be run on Mac OS, you can use the ‘zap.sh’ script, as per linux.
+
+This script has been copied from the ZAP's installation directory,
+i.e. "/Applications/OWASP\ ZAP.app/Contents/Java/zap.sh".
+and it is from ZAP's version 2.13.0.
+
+And the zap-2.13.0.jar file can be copied from "/Applications/OWASP\ ZAP.app/Contents/Java/zap-2.13.0.jar"
+
+
+
+```
+docker pull ghcr.io/zaproxy/zaproxy:stable
+or
+docker pull softwaresecurityproject/zap-stable
+```
--- src/test/java/penetration/pk/lucidxpo/ynami/zap/zap.sh
+++ src/test/java/penetration/pk/lucidxpo/ynami/zap/zap.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# https://www.zaproxy.org/faq/how-can-you-start-zap/
+#
+# Generally, most user’s tend to use the Mac OS build, which is a ordinary Mac OS app that can be started as any
+# other app: Double-Click on the app to start it.
+#
+#If you have installed ZAP in ‘/Applications’ then you can run it from the command line using
+# ‘/Applications/OWASP\ ZAP.app/Contents/Java/zap.sh’
+#
+#If you have installed ZAP in another directory then change the initial ‘/Applications’ part accordingly.
+#
+#If you have downloaded the Linux package, which can also be run on Mac OS, you can use the ‘zap.sh’ script, as per linux.
+
+# This script has been copied from the ZAP's installation directory,
+# i.e. "/Applications/OWASP\ ZAP.app/Contents/Java/zap.sh".
+# and it is from ZAP's version 2.13.0.
+#
+# And the zap-2.13.0.jar file can be copied from "/Applications/OWASP\ ZAP.app/Contents/Java/zap-2.13.0.jar"
+
 # Dereference from link to the real directory
 SCRIPTNAME="$0"
 
@@ -36,19 +54,21 @@ JAVA_OUTPUT=$(java -version 2>&1)
 # Catch warning: Unable to find a $JAVA_HOME at "/usr", continuing with system-provided Java
 if [ "`echo ${JAVA_OUTPUT} | grep "continuing with system-provided Java"`" ] ; then
   echo "WARNING, \$JAVA_HOME could be set incorrectly, Java's error is:"
-  echo "    " ${JAVA_OUTPUT}
+  echo "    " $JAVA_OUTPUT
   echo "Unsetting JAVA_HOME and continuing with ZAP start-up"
   unset JAVA_HOME
 fi
 
+DEFAULTJAVAGC=""
+
 JAVA_VERSION=$(java -version 2>&1 | awk -F\" '/version/ { print $2 }')
-JAVA_MAJOR_VERSION=${JAVA_VERSION%%.*}
+JAVA_MAJOR_VERSION=${JAVA_VERSION%%[.|-]*}
 JAVA_MINOR_VERSION=$(echo $JAVA_VERSION | awk -F\. '{ print $2 }')
 
-if ([ $JAVA_MAJOR_VERSION -ge 1 ] && [ $JAVA_MINOR_VERSION -ge 7 ]) || [ $JAVA_MAJOR_VERSION -ge 10 ]; then
+if [ $JAVA_MAJOR_VERSION -ge 11 ]; then
   echo "Found Java version $JAVA_VERSION"
 else
-  echo "Exiting: ZAP requires a minimum of Java 7 to run, found $JAVA_VERSION"
+  echo "Exiting: ZAP requires a minimum of Java 11 to run, found $JAVA_VERSION"
   exit 1
 fi
 
@@ -73,11 +93,14 @@ elif [ "$OS" = "FreeBSD" ]; then
 fi
 
 if [ ! -z "$JMEM" ]; then
-  echo "Using jvm memory setting from $JVMPROPS"
+  echo "Read custom JVM args from $JVMPROPS"
+  JAVAGC=""
 elif [ -z "$MEM" ]; then
   echo "Failed to obtain current memory, using jvm default memory settings"
+  JAVAGC=${DEFAULTJAVAGC}
 else
   echo "Available memory: $MEM MB"
+  JAVAGC=${DEFAULTJAVAGC}
   if [ "$MEM" -gt 512 ]; then
     # Always go with 1/4 of the available memory - specific JVMs may round this up or down
     QMEM=$(($MEM/4))
@@ -90,6 +113,12 @@ for var in "$@"; do
   if [[ "$var" == -Xmx* ]]; then
     # Overridden by the user
     JMEM="$var"
+  elif [[ $var == --jvmdebug* ]]; then
+	JAVADEBUGPORT=`echo "$var" | sed -e "s/--jvmdebug//g" | sed -e "s/=//g"`
+	if [ ! "$JAVADEBUGPORT" ]; then
+		JAVADEBUGPORT=1044
+	fi
+	JAVADEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:$JAVADEBUGPORT"
   elif [[ $var != -psn_* ]]; then
     # Strip the automatic -psn_x_xxxxxxx argument that OS X automatically passes into apps, since
     # it freaks out ZAP
@@ -99,13 +128,18 @@ done
 
 if [ -n "$JMEM" ]
 then
-  echo "Setting jvm heap size: $JMEM"
+  echo "Using JVM args: $JMEM"
+fi
+
+if [ -n "$JAVADEBUG" ]
+then
+  echo "Setting debug: $JAVADEBUG"
 fi
 
 # Start ZAP; it's likely that -Xdock:icon would be ignored on other platforms, but this is known to work
 if [ "$OS" = "Darwin" ]; then
   # It's likely that -Xdock:icon would be ignored on other platforms, but this is known to work
-  exec java ${JMEM} -Xdock:icon="../Resources/ZAP.icns" -jar "${BASEDIR}/zap-2.6.0.jar" "${ARGS[@]}"
+  exec java ${JMEM} ${JAVAGC} ${JAVADEBUG} -Xdock:icon="../Resources/ZAP.icns" -jar "${BASEDIR}/zap-2.13.0.jar" "${ARGS[@]}"
 else
-  exec java ${JMEM} -jar "${BASEDIR}/zap-2.6.0.jar" "${ARGS[@]}"
+  exec java ${JMEM} ${JAVAGC} ${JAVADEBUG} -jar "${BASEDIR}/zap-2.13.0.jar" "${ARGS[@]}"
 fi
