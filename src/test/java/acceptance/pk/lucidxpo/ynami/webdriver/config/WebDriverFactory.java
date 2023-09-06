e5d9c5e639565926fc6178985478556917459528
Crazy <faisalazam_1616@hotmail.com>
2023-09-07 01:00:02 +0500

    Moved chrome webdriver construction logic to a new ChromeWebDriverBuilder class.

--- src/test/java/acceptance/pk/lucidxpo/ynami/webdriver/config/WebDriverFactory.java
+++ src/test/java/acceptance/pk/lucidxpo/ynami/webdriver/config/WebDriverFactory.java
@@ -1,42 +1,11 @@
 package acceptance.pk.lucidxpo.ynami.webdriver.config;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
-import static java.lang.Boolean.TRUE;
-import static java.lang.Boolean.valueOf;
-import static java.lang.System.getenv;
+import pk.lucidxpo.ynami.utils.webdrivers.ChromeWebDriverBuilder;
 
 public class WebDriverFactory {
-    private static final String TEST_TYPE = "--test-type";
-    private static final String HEADLESS = "--headless=new";
-    private static final String NO_SANDBOX = "--no-sandbox";
-    private static final String HEADLESS_MODE = "headless_mode";
-    private static final String START_MAXIMIZED = "--start-maximized";
-    private static final String DISABLE_DEV_SHM_USAGE = "--disable-dev-shm-usage";
-
     public static WebDriver getDriver() {
-        final WebDriverManager webDriverManager = chromedriver();
-        final ChromeOptions options = new ChromeOptions();
-        options.setAcceptInsecureCerts(true);
-        options.addArguments(
-                TEST_TYPE,
-                NO_SANDBOX,
-                START_MAXIMIZED,
-                DISABLE_DEV_SHM_USAGE
-        );
-
-        // default setting will be headless mode
-        final String headlessMode = getenv(HEADLESS_MODE);
-        if (headlessMode == null || TRUE.equals(valueOf(headlessMode))) {
-            options.addArguments(HEADLESS);
-        }
-        webDriverManager
-                .capabilities(options)
-                .setup();
-        return webDriverManager.create();
+        return new ChromeWebDriverBuilder().build();
     }
 
 //    private WebDriverFactory() {
---
+++ src/test/java/pk/lucidxpo/ynami/utils/webdrivers/ChromeWebDriverBuilder.java
@@ -0,0 +1,68 @@
+package pk.lucidxpo.ynami.utils.webdrivers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+import static java.lang.Boolean.TRUE;
+import static java.lang.Boolean.valueOf;
+import static java.lang.System.getenv;
+import static java.time.Duration.ofSeconds;
+
+public class ChromeWebDriverBuilder {
+    private final static int TIMEOUT = 10;
+    private static final String CI = "CI";
+    private static final String HEADLESS = "--headless";
+    private static final String TEST_TYPE = "--test-type";
+    private static final String ENVIRONMENT = "environment";
+    private static final String NO_SANDBOX = "--no-sandbox";
+    private static final String HEADLESS_MODE = "headless_mode";
+    private static final String START_MAXIMIZED = "--start-maximized";
+    private static final String DISABLE_DEV_SHM_USAGE = "--disable-dev-shm-usage";
+
+    public WebDriver build() {
+        final String environment = getenv(ENVIRONMENT);
+        final ChromeOptions options = getChromeOptions();
+        if (CI.equalsIgnoreCase(environment)) {
+            // Tests are not passing when the webDriver is managed through WebDriverManager on Linux/local Docker
+            // container/GitHub Workflow, but all fine on local macOS. WebDriverManager is good in a way that we don't
+            // need to download/setup webdriver path, and it is doing all that fine on all systems, but for some reason
+            // it results in a NullPointerException during webElement interactions.
+            final WebDriver driver = new ChromeDriver(options);
+            // This 'implicitlyWait' is an important statement for the proper functioning of the cucumber tests on linux
+            // machine. Tests were failing only on Linux/GitHub Workflow and nothing worked even after trying like million
+            // of things. They work fine on my local Mac but may be the other machines or too fast or too slow that they
+            // keep failing there. 'implicitlyWait' is here for the rescue finally.
+            driver.manage().timeouts().implicitlyWait(ofSeconds(TIMEOUT));
+            return driver;
+        }
+
+        final WebDriverManager webDriverManager = chromedriver();
+        webDriverManager
+                .capabilities(options)
+                .setup();
+        final WebDriver driver = webDriverManager.create();
+        driver.manage().timeouts().implicitlyWait(ofSeconds(TIMEOUT));
+        return driver;
+    }
+
+    private static ChromeOptions getChromeOptions() {
+        final ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        options.addArguments(
+                TEST_TYPE,
+                NO_SANDBOX,
+                START_MAXIMIZED,
+                DISABLE_DEV_SHM_USAGE
+        );
+
+        // default setting will be headless mode
+        final String headlessMode = getenv(HEADLESS_MODE);
+        if (headlessMode == null || TRUE.equals(valueOf(headlessMode))) {
+            options.addArguments(HEADLESS);
+        }
+        return options;
+    }
+}
