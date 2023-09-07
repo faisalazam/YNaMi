e80e57d7832856e3c221c337a58955e4456d0be6
Crazy <faisalazam_1616@hotmail.com>
2023-09-07 17:45:12 +0500

    Removed quitAll(); and stopZap(); as they've moved to their own hooks.

--- src/test/java/penetration/pk/lucidxpo/ynami/test/SecurityTest.java
+++ src/test/java/penetration/pk/lucidxpo/ynami/test/SecurityTest.java
@@ -62,8 +62,6 @@ public class SecurityTest {
         // TODO: do I need to generate reports explicitly? I think the ones generated through maven will be good enough.
         //  Or consider combining the reports from cluecumber-report-plugin and maven-cucumber-reporting plugins.
         generateReports(CUCUMBER_HTML_REPORTS_PATH, CUCUMBER_JSON_REPORT_PATH);
-        quitAll();
-        stopZap();
 
 //    TODO: See how we can enable the following method which got commented after all these upgrades???
 //    connection is coming from CucumberContextConfigurationLoader
